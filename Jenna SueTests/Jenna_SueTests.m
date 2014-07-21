f0c5c8cbc30aad635d32d19c2343949ab4161fc9
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-07-21 11:45:54 +0100

    Initial Commit

---
+++ Jenna SueTests/Jenna_SueTests.m
@@ -0,0 +1,34 @@
+//
+//  Jenna_SueTests.m
+//  Jenna SueTests
+//
+//  Created by Gabriel Massana on 21/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Jenna_SueTests : XCTestCase
+
+@end
+
+@implementation Jenna_SueTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
